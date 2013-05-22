26a5e9d4742da111bb04537e5eaac232d6dabea4
Bill Schofield <bschofield@yahoo.com>
2013-05-22 17:28:50 -0500

    Starting point

---
+++ src/com/thoughtworks/numbers/Numbers.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.numbers;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+// User stories
+// Print the differences between each pair of numbers that are neighbors. For instance, "1 - 9 = -8; 9 - 4 = 5; 4 - 16 = -12"
+// Print out unique numbers and their frequency (number of times they occur. "[1, 1], [9, 1], [4, 2], [16, 1]"
+// Print the square root of unique numbers in ascending order
+
+public class Numbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = newArrayList(1, 9, 4, 16, 4);
+
+        List<Integer> differences = new DifferenceCalculator().findDifferences(numbers);
+        printList(differences);
+    }
+
+    private static void printList(List<Integer> numbers) {
+
+    }
+}
