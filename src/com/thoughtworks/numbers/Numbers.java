0e22ce37ca920d22240c7a09fccc0aef4e3dbe67
Bill Schofield <bill@billschofield.net>
2013-05-24 14:23:44 -0500

    Changed problems to only use filter and transform

--- src/com/thoughtworks/numbers/Numbers.java
+++ src/com/thoughtworks/numbers/Numbers.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-// Given a list of integers...
-// Print the mean and standard deviation of the numbers
-// Print out unique numbers and their frequency (number of times they occur.
-// "[1, 1], [9, 1], [4, 2], [16, 1]"
+// Given a list of numbers like {1, 9, 4, 16, 4}
+// Print the square root of all of the numbers larger than 4.
+// For example, "3^2 = 9, 4^2 = 16"
+
+// Given a list of strings like {"Bill", "Archer", "Lana"}
+// Print the first letter of each of the string that have a length of 4.
+// For example, "BL"
 
 public class Numbers {
     public static void main(String[] args) {
