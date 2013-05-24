333cd33bb9968997ac0652ce6248ebab8daecfae
Bill Schofield <bill@billschofield.net>
2013-05-24 13:54:45 -0500

    Removed square root and difference problems and added mean and std dev

--- src/com/thoughtworks/numbers/DifferenceCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.numbers;
-
-import java.util.List;
-
-public class DifferenceCalculator {
-    public List<Integer> findDifferences(List<Integer> numbers) {
-        return numbers;
-    }
-}
