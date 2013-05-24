46def8011cad2d5602071c81099c9ac041a20f11
Bill Schofield <bill@billschofield.net>
2013-05-24 13:45:19 -0500

    Changed difference problem to mean and std dev

--- src/com/thoughtworks/numbers/Numbers.java
+++ src/com/thoughtworks/numbers/Numbers.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 import static java.util.Collections.addAll;
 
-// User stories
-// Print the differences between each pair of numbers that are neighbors. For instance, "-8, 5, -12"
+// Given a list of integers...
+// Print the mean and standard deviation of the numbers
 // Print out unique numbers and their frequency (number of times they occur. "[1, 1], [9, 1], [4, 2], [16, 1]"
-// Print the square root of unique numbers in ascending order
 
 public class Numbers {
     public static void main(String[] args) {
@@ -16,12 +15,6 @@ public class Numbers {
         List<Integer> numbers = new ArrayList<Integer>();
         addAll(numbers, source);
 
-        List<Integer> differences = new DifferenceCalculator().findDifferences(numbers);
-        printList(differences);
-
     }
 
-    private static void printList(List<Integer> numbers) {
-
-    }
 }
